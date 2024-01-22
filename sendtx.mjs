14fd400ad4c2d067bfdcc82eebb5719c366700eb
Winter <huangdongdong910@163.com>
2024-01-22 14:28:54 +0800

    upadte

---
+++ sendtx.mjs
@@ -0,0 +1,65 @@
+//const {Web3} = require('web3');
+import Web3 from 'web3';
+import cryptoRandomString from 'crypto-random-string';
+
+// 连接到以太坊节点
+const web3 = new Web3('http://54.175.105.94:8545');
+
+const privateKey = '0xc669bfc9772541b997cf9a3f11911c482b1c55540599205f45fdb518dc891637';
+const senderAddress = '0x273Ceb0D6A0AFc8316eb92c254ce332fB75268fb';
+const recipientAddress = '0x20424d513DFe18cab20F78b1c6B8135B138674B4';
+
+const createTransaction = async (nonce) => {
+    try {
+        const balance = await web3.eth.getBalance(senderAddress);
+        console.log("balance:", balance)
+
+        const baseFee = await web3.eth.getGasPrice(); // 获取当前区块的基础燃气费
+        //console.log("baseFee:", baseFee)
+
+        const adjustedGasPrice = web3.utils.toWei((parseInt(baseFee) + 1).toString(), 'wei');
+        //console.log("adjustedGasPrice", adjustedGasPrice)
+
+        // 创建交易对象
+        const transactionObject = {
+            from: senderAddress,
+            to: recipientAddress,
+            value: "1",
+            //gas: 21000,
+            gasPrice: adjustedGasPrice,
+            nonce: nonce, // 使用获取到的最新 nonce
+            data:'0x'+cryptoRandomString({ length: 131000 })   //'A'.repeat(131000)
+        };
+      //console.log('data:', transactionObject.data)
+      const estimate = await web3.eth.estimateGas(transactionObject);
+      console.log("estimate: ", estimate);
+      transactionObject.gas = estimate;
+      // 使用私钥进行签名
+      const signedTransaction = await web3.eth.accounts.signTransaction(transactionObject, privateKey);
+      // 发送已签名的交易
+      console.log('send tx: ', 'nonce is ', nonce)
+      const receipt = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
+      console.log('tx is succeeded :', "nonce is ",nonce, "tx hash is ", receipt.transactionHash);
+    } catch (error) {
+      console.error('tx is failed:', error);
+    }
+  };
+
+async function main(){
+  try {
+    var nonce = await web3.eth.getTransactionCount(senderAddress);
+    while(true){
+      createTransaction(nonce);
+      nonce++
+      await sleep(25)
+    }
+  } catch (error) {
+    console.error("something error: ", error)
+  }
+}
+
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+main()
